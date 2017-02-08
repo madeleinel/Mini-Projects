1e847d55d91abb9a4b36fed9a0750cfa70366dba
madeleinel <madeleinelinder@hotmail.com>
2017-02-08 14:33:37 +0000

    updated html canvas with working effect choices

--- JS30/htmlCanvas-WIP/script.js
+++ JS30/htmlCanvas-WIP/script.js
@@ -1,6 +1,5 @@
 // Set const "canvas" to be the canvas div with id "draw"
 const canvas = document.querySelector("#draw");
-
 // Set context to be 2d (?), getting the data from the "canvas" variable
 const ctx = canvas.getContext("2d");
 
@@ -9,32 +8,25 @@ canvas.width = (window.innerWidth * 0.98);
 canvas.height = (window.innerHeight * 0.8);
 
 // Set the default colour of the brushstrokes
-// ctx.strokeStyle = "tomato";
-
+    // ctx.strokeStyle = "tomato";
 // Set the default style of brushstoke joints
 ctx.lineJoin = "round";
-
 // Set the default style of brushstoke ends
 ctx.lineCap = "round";
-
 // Set the width of the brush strokes
 ctx.lineWidth = 10;
-
 // Set the colour to combine with existing colour when doubling over previous brush strokes
 ctx.globalCompositeOperation = 'source-over';
 // console.log(ctx.globalCompositeOperation);
 
-// Set
 //// Set variables as 'let', as the values of these variables will change when moving the mouse/painting on the canvas
-
 let isDrawing = false;  // Set default state to be not-drawing (ie when not pressing down the mouse)
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
 let direction = true;
 let newColor = "tomato";
-let newWidth = 100;
-// let newEffect = "source-over";
+let newWidth = 10;
 
 const colorChange = document.getElementById('colorSelector');
 const widthChange = document.getElementById('widthSelector');
@@ -44,32 +36,54 @@ const widthChange = document.getElementById('widthSelector');
 colorChange.addEventListener('change', (e) => {
   newColor = e.srcElement.value;
 });
-
 widthChange.addEventListener('change', (e) => {
   newWidth = e.srcElement.value;
 });
 
-const effectChange = document.getElementsByClassName('effectValue');
-const effectList = document.querySelector('.effectList');
-console.log(effectChange);
 
-// var changeEffect = function () {
-  for (const value of effectChange) {
-    if (value.checked === true) {
-      effectList.addEventListener('checked', (e) => {
-        ctx.globalCompositeOperation = value.value; // value.value == the name of the chosen effect
-        console.log(ctx.globalCompositeOperation);
-      });
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
+
+const effectList = document.getElementById('effectList');
+// console.log(effectList);
+
+const effectOptions = document.querySelectorAll('#effectList input');
+// console.log(effectOptions);
+
+const nOfEffects = effectOptions.length;
+const effectValues = document.querySelectorAll('.effectValue');
+// console.log(effectValues);
+
+// Listen for mousedown/clicking within the list of effect options
+effectList.addEventListener('change', (e) => {
+
+  // When click within the list >> loop through all items within the list
+  for (i = 0; i < nOfEffects; i++) {
+    // console.log(effectOptions[i].checked);
+
+    // check whether they were the option that was chosen
+    // if yes >> set 'ctx.globalCompositeOperation' to that value
+    if (effectOptions[i].checked === true) {
+      // console.log(effectOptions[i].value);
+
+      ctx.globalCompositeOperation = effectOptions[i].value;
+      // console.log(ctx.globalCompositeOperation);
     }
   }
-// }
+});
 
-// try adding an event to each element & listen to them, then link them to that function
+
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
 
 function draw(e) {
   if(!isDrawing) return;  // if currently not-drawing, stop the function from running
-
-  // console.log(e);
 
   ctx.strokeStyle = newColor;
   // `hsl(${hue}, 100%, 50%)`
@@ -94,12 +108,10 @@ function draw(e) {
 }
 
 // Add event listeners to tell the [program] when drawing is happening and not
-
 canvas.addEventListener('mousedown', (e) => { // when mouse-down >> means it's drawing on the canvas
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
-
 canvas.addEventListener('mousemove', draw);
 // canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);  // when mouse-up >> means no longer drawing on the canvas
